8c254e455a90d3b1491766d453bee61ffa61a77c
Blaster <blasterbuild.161317@gmail.com>
2023-06-09 03:54:36 +0530

    worked on frontend

--- my_app.py
+++ my_app.py
@@ -2,16 +2,24 @@ import streamlit as st
 import numpy as np
 import pickle
 import nltk
-import contractions,string
+import contractions, string
 import pandas
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
+from PIL import Image
+import time
+
+
+
+
 
 # loading the saved model
 loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 loaded_tfidf = pickle.load(open('tfidf_v.sav', 'rb'))
 st.title("Product review sentiment analysis")
-input_rev = st.text_area("Enter your review please")
+input_rev = st.text_area("", height=10, placeholder='write your review here')
+
+
 
 
 def space_contractions(text):
@@ -40,6 +48,7 @@ def remove_stopwords(data):
             swords.append(stopword)
     return " ".join(swords)
 
+
 list = []
 stemmer = PorterStemmer()
 
@@ -49,56 +58,49 @@ def stemming(text):
         list.append(stemmer.stem(word))
     return " ".join(list)
 
-if st.button("analysis"):
 
-#preprocessing
-
-    #contractions
+if st.button("Predict", use_container_width=True):
+    prgress_text = "Operation in progress please wait"
+    my_bar = st.progress(0, text=prgress_text)
+    time.sleep(0.1)
+    my_bar.progress(50, text=prgress_text)
+    time.sleep(0.1)
+    my_bar.progress(100, text=prgress_text)
+    # preprocessing
+    # contractions
     input_rev = space_contractions(input_rev)
-    #lowercase
+    # lowercase
     input_rev = input_rev.lower()
-    #removing punctuations
+    # removing punctuations
     input_rev = remove_punctuation(input_rev)
-    #remove stopwords
+    # remove stopwords
     input_rev = remove_stopwords(input_rev)
-    #stemming
+    # stemming
     input_rev = stemming(input_rev)
     input_rev = [input_rev]
 
-
-# vectorize
+    # vectorize
 
     tfidf_vectorize = loaded_tfidf.transform(input_rev)
 
-# predict
+    # predict
 
     result = loaded_model.predict(tfidf_vectorize)
 
+
 # display
 
-if(result == 1):
-    st.header("Positive")
+
+
+
+
+if (result == 1):
+    st.header(':blue[Positive] :sunglasses:')
+    st.balloons()
+
+
+    def check():
+        return True
 else:
-    st.header("Negative")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    st.header(':red[Negative]')
+    st.snow()
